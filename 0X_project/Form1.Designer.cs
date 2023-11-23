41c8166b2b543c5bef5bbd3aded872cdec312035
muslinazavtrak@gmail.com <muslinazavtrak@gmail.com>
2023-11-23 15:48:29 +0300

    Добавьте файлы проекта.

---
+++ 0X_project/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace _0X_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.Restart = new System.Windows.Forms.Button();
+            this.turn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.button1.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.Location = new System.Drawing.Point(70, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(176, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(282, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(70, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(176, 135);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Location = new System.Drawing.Point(282, 135);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(70, 241);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Location = new System.Drawing.Point(176, 241);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Matura MT Script Capitals", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Location = new System.Drawing.Point(282, 241);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Restart.Location = new System.Drawing.Point(633, 41);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(103, 42);
+            this.Restart.TabIndex = 9;
+            this.Restart.Text = "Restart";
+            this.Restart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // turn
+            // 
+            this.turn.AutoSize = true;
+            this.turn.Font = new System.Drawing.Font("Microsoft New Tai Lue", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turn.Location = new System.Drawing.Point(451, 369);
+            this.turn.Name = "turn";
+            this.turn.Size = new System.Drawing.Size(82, 38);
+            this.turn.TabIndex = 10;
+            this.turn.Text = "Turn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.turn);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Label turn;
+    }
+}
+
---
+++ 0X_project/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0X_project
+{
+    public partial class Form1 : Form
+    {
+        string _side = ""; 
+        Button[] _btn = new Button[9];
+        int[] _state = new int[9]; 
+        Random _turn = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            if (_turn.Next(1, 10) <= 5)
+            {
+                _side = "крестики";
+                turn.Text = "Ходят крестики!";
+            }
+            else
+            {
+                _side = "нолики";
+                turn.Text = "Ходят нолики!";
+            }
+            _btn[0] = button1;
+            _btn[1] = button2;
+            _btn[2] = button3;
+            _btn[3] = button4;
+            _btn[4] = button5;
+            _btn[5] = button6;
+            _btn[6] = button7;
+            _btn[7] = button8;
+            _btn[8] = button9;
+
+        }
+        private void Win(int startItem, int step, string player)
+        {
+            if(player!="ничья")
+            {
+                _btn[startItem].BackColor = Color.ForestGreen;
+                _btn[startItem + step].BackColor = Color.ForestGreen;
+                _btn[startItem + 2 * step].BackColor = Color.ForestGreen;
+            }
+            else
+            {
+                for (int i = 0; i <_btn.Length; i++)
+                    _btn[i].BackColor = Color.ForestGreen;
+            }
+            for (int i = 0; i < _btn.Length; i++)
+                _btn[i].Enabled = false;
+            Restart.Enabled = true;
+            MessageBox.Show( $"Победили {player} !!!");
+
+        }
+        private bool DrawGame()
+        {
+            int counter = 0;
+            for (int i = 0; i < _btn.Length; i++)
+                if (!_btn[i].Enabled)
+                    counter++;
+            if (counter == 9) 
+                return true;
+            else
+                return false;
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if(_side == "крестики")
+            {
+                for (int i = 0; i < _btn.Length; i++)
+                {
+                    if(sender == _btn[i] )
+                    {
+                        _btn[i].Text = "X";
+                        turn.Text = "Ходят нолики!";
+                        _side = "нолики";
+                        _btn[i].Enabled = false;
+                        _state[i] = 1;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _btn.Length; i++)
+                {
+                    if (sender == _btn[i])
+                    {
+                        _btn[i].Text = "0";
+                        turn.Text = "Ходят крестики!";
+                        _side = "крестики";
+                        _btn[i].Enabled = false;
+                        _state[i] = 2;
+                        break;
+
+                    }
+                }
+            }
+            if (_state[0] == 1 && _state[1] == 1 && _state[2] == 1)
+                Win(0, 1, "крестики");
+            else if (_state[3] == 1 && _state[4] == 1 && _state[5] == 1)
+                Win(3, 1, "крестики");
+            else if (_state[6] == 1 && _state[7] == 1 && _state[8] == 1)
+                Win(6, 1, "крестики");
+            else if (_state[0] == 1 && _state[3] == 1 && _state[6] == 1)
+                Win(0, 3, "крестики");
+            else if (_state[1] == 1 && _state[4] == 1 && _state[7] == 1)
+                Win(1, 3, "крестики");
+            else if (_state[2] == 1 && _state[5] == 1 && _state[8] == 1)
+                Win(2, 3, "крестики");
+            else if (_state[0] == 1 && _state[4] == 1 && _state[8] == 1)
+                Win(0, 4, "крестики");
+            else if (_state[2] == 1 && _state[4] == 1 && _state[6] == 1)
+                Win(2, 2, "крестики");
+            else if (_state[0] == 2 && _state[1] == 2 && _state[2] == 2)
+                Win(0, 1, "нолики");
+            else if (_state[3] == 2 && _state[4] == 2 && _state[5] == 2)
+                Win(3, 1, "нолики");
+            else if (_state[6] == 2 && _state[7] == 2 && _state[8] == 2)
+                Win(6, 1, "нолики");
+            else if (_state[0] == 2 && _state[3] == 2 && _state[6] == 2)
+                Win(0, 3, "нолики");
+            else if (_state[1] == 2 && _state[4] == 2 && _state[7] == 2)
+                Win(1, 3, "нолики");
+            else if (_state[2] == 2 && _state[5] == 2 && _state[8] == 2)
+                Win(2, 3, "нолики");
+            else if (_state[0] == 2 && _state[4] == 2 && _state[8] == 2)
+                Win(0, 4, "нолики");
+            else if (_state[2] == 2 && _state[4] == 2 && _state[6] == 2)
+                Win(2, 2, "нолики");
+            else if (DrawGame())
+                Win(0, 0, "ничья");
+        }
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < _btn.Length; i++)
+            {
+                _btn[i].Enabled = true;
+                _btn[i].Text = "";
+                _btn[i].BackColor = Color.White;
+                _state[i] = 0;
+
+                if (_turn.Next(1, 10) <= 5)
+                {
+                    _side = "крестики";
+                    turn.Text = "Ходят крестики!";
+                }
+                else
+                {
+                    _side = "нолики";
+                    turn.Text = "Ходят нолики!";
+                }
+            }
+        }
+    }
+}
